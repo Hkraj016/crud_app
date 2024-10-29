aa210c982c568a6e53041b39e054278c985eb28f
Hkraj016 <raj016.hk@gmail.com>
2024-10-30 00:52:24 +0530

    Read_Delete  in table using Tailwind with Sample JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductTableComponent } from './product-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'crud_app';
+
 }
